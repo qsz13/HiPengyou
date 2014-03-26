56d30d8b7ec604460843429f97a7cf31fa743c87
qsz13 <qsz1328@gmail.com>
2014-03-26 11:19:31 +0800

    Initial Commit

---
+++ HiPengyou/HPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPAppDelegate.h
+//  HiPengyou
+//
+//  Created by Daniel Qiu on 3/26/14.
+//  Copyright (c) 2014 HiPengyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HiPengyou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HiPengyou
+//
+//  Created by Daniel Qiu on 3/26/14.
+//  Copyright (c) 2014 HiPengyou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPAppDelegate class]));
+    }
+}
